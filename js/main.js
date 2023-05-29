8ebdc0783539eac20337612541d6eb62c7ec7e8f
Martinraju11 <martinraju53@gmail.com>
2023-05-29 11:32:30 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+var n=parseInt(prompt("Enter the number of rows of pattern(1-9)"));
+for(let i=1;i<=n;i++)
+{
+	for(let j=n;j>i;j--)
+	{
+		document.write("*");
+		document.write("&nbsp;");
+	}
+	
+	for(let k=1;k<=i;k++)
+	{
+		document.write(i);
+		document.write("&nbsp;");
+	}
+	document.write("<br>");
+}
+	
